1a3f9157e9019f7b0a9542b63234f51a9506c4ff
ramonrivasvarela <122489640+ramonrivasvarela@users.noreply.github.com>
2025-06-27 16:45:22 -0500

    instrument changes

--- instrument_activation.py
+++ instrument_activation.py
@@ -1,4 +1,4 @@
-pulser_activation_boolean=False
-xyz_activation_boolean=False
-sg_activation_boolean=False
+pulser_activation_boolean=True
+xyz_activation_boolean=True
+sg_activation_boolean=True
 dlnsec_activation_boolean=False
